083d8d0d16d90eb212c6266f785affb186b178d5
Corey <corey@CoreydeMBP.lan>
2019-05-30 23:39:56 +0800

    add numpyOP2

---
+++ DataAnalysis/numpyOP2.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from matplotlib import pyplot as plt
+
+file_path = '/Users/corey/Tmp/GB_video_data_numbers.csv'
+
+
+def test():
+    t1 = np.loadtxt(file_path, delimiter=',', dtype='int')
+
+    print(t1[:,-1])
+    plt.figure(figsize=(20,8),dpi=144)
+    t1 = t1[t1<=5000]
+    num_bins = int((t1.max()-t1.min())//50)
+    print(num_bins)
+    plt.hist(t1[:-1],num_bins)
+    plt.show()
+
+def test2():
+    t1 = np.loadtxt(file_path, delimiter=',', dtype='int')
+    plt.figure(figsize=(20, 8), dpi=144)
+    t1= t1[t1[:,1]<=500000]
+    t1_like = t1[:,1]
+    t1_comment = t1[:,-1]
+    plt.scatter(t1_like,t1_comment)
+    plt.show()
+
+
+def main():
+    test2()
+    # 布尔索引
+    # t = np.arange(24).reshape(4, 6)
+    # # 将t中小于10的数据设置成0
+    # t[t < 10] = 0
+    # print(t)
+    # # 用3元运算符将小于10的设置成0 大于10的设置成10
+    # np.where(t < 10, 0, 10)
+    # print(t)
+    # # 用clip方法，将小于3的值设置为3 大于18的值设置为18
+    # t = np.clip(t, 3, 18)
+    # print(t)
+    # # 求和
+    # print(t.sum())
+    # # 求平均值
+    # print(t.mean())
+    # # 求中值
+    # print(np.median(t))
+    # # 求最大值和最小值
+    # print(t.max())
+    # print(t.min())
+    # # 求极差
+    # print(np.ptp(t))
+    # # 求标准差 标准差越大 这集合中大部分数据和平均值差异很大
+    # print(t.std())
+
+
+if __name__ == "__main__":
+    main()
